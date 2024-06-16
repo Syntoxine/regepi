f7bd4322924ffc5fb792325712356f6656501a57
Flavien Dussud <91691572+Syntoxine@users.noreply.github.com>
2024-06-16 22:08:37 +0200

    separated stylesheets to work on per page basis (issues with loading page stylesheet though)

--- firefox/accueil.js
+++ firefox/accueil.js
@@ -1,7 +1,9 @@
+// Remove undesirables
 let alert_div = document.getElementById("temoin_messagerie_non_vide");
 let button_divs = document.querySelector("#temoin_messagerie_non_vide + div");
 
-alert_div.style.display = "none";
-button_divs.style.display = "none";
+[alert_div, button_divs].forEach(element => {
+    element.style.display = "none";
+});
 
-console.log("accueil.js successfully loaded.")
+console.log("accueil.js successfully loaded.");
--- firefox/main.js
+++ firefox/main.js
@@ -12,19 +12,30 @@ document.head.appendChild(inter_stylesheet);
 // Add main ReGEPI stylesheet
 const stylesheet = document.createElement('link');
 stylesheet.rel = 'stylesheet';
-stylesheet.type = 'text/css';
 stylesheet.href = browser.runtime.getURL('main.css');
 
 document.head.appendChild(stylesheet);
 
-// Add correspond page stylesheet
+// Add correspond page script and stylesheet
 const page_script = document.createElement('script');
+const page_stylesheet = document.createElement('link');
+page_stylesheet.rel = 'stylesheet';
 page_script.type = 'text/javascript';
 
 let match = document.location.pathname.match(/\/([^\/]+)\.php$/);
 if (match) {
     page_script.src = browser.runtime.getURL(`${match[1]}.js`);
+    page_stylesheet.href = browser.runtime.getURL(`${match[1]}.css`);
     document.head.appendChild(page_script);
+    document.head.appendChild(page_stylesheet);
 } else {
     console.log("Unsupported page (Non-PHP).")
 }
+
+// Remove undesirables
+let minimize_button = document.querySelector(".change_taille_gd");
+minimize_button.style.display = "none";
+
+let alert_heading = document.querySelector("h1 + h3");
+alert_heading.classList.add("alert");
+alert_heading.textContent = alert_heading.textContent.split(' ')[0];
