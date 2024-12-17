e0b13761aa2e785a37dd85e8252ddfb2186f912f
Flavien Dussud <flavien@dussud.org>
2024-12-17 16:55:45 +0100

    exported style component to another file

--- firefox/scripts/accueil.js
+++ firefox/scripts/accueil.js
@@ -1,17 +1,1 @@
-// // Remove undesirables
-// let alert_div = document.getElementById("temoin_messagerie_non_vide");
-// let button_divs = document.querySelector("#temoin_messagerie_non_vide + div");
-
-// [alert_div, button_divs].forEach(element => {
-//     element.style.display = "none";
-// });
-
-
-// // Remove last connection
-// let last_conection = document.querySelector(".colonne1");
-// last_conection.style.display = "none";
-
-
-
-
 console.log("accueil.js successfully loaded.");
