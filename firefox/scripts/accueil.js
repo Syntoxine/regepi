3d35c8b397c9f77d42b2b78efa5b4971e364ec22
Matthias Behrens <m.behrens107@gmail.com>
2025-01-07 17:35:59 +0100

    Ajout Navbar

--- firefox/scripts/accueil.js
+++ firefox/scripts/accueil.js
@@ -1,1 +1,5 @@
 console.log("accueil.js successfully loaded.");
+const menuHamburger = document.querySelector(".menu-hamburger");
+const navLinks = document.querySelector(".nav-links");
+
+menuHamburger.addEventListener('click', ()=>{navLinks.classList.toggle('mobile-menu')});
