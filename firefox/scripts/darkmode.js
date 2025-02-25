77335a7d2185a18a5b9a7f4d36be25e8a8092bc7
Matthias Behrens <m.behrens107@gmail.com>
2025-02-25 12:13:07 +0100

    commentaires sur darkmode + importation palette dans accueil.css (à modifier après)

--- firefox/scripts/darkmode.js
+++ firefox/scripts/darkmode.js
@@ -1,18 +1,28 @@
+// Récupere le thème actuel du stockage local, si le mode sombre est stocké, alors il sera appliqué
 let darkmode = localStorage.getItem('darkmode')
+
 const themeSwitch = document.getElementById('theme-switch')
 
+// Ajoute la classe 'darkmode' aux classes du 'body'
+// Enregistre le mode sombre comme actif dans le stockage local
 const enableDarkmode = ()=>{
     document.body.classList.add('darkmode')
     localStorage.setItem('darkmode', 'active')
 }
 
+// Retire la classe 'darkmode' aux classes du 'body'
+// Enregistre le mode sombre comme non-présent dans le stockage local
 const disableDarkmode = ()=>{
     document.body.classList.remove('darkmode')
     localStorage.setItem('darkmode', null)
 }
 
+// Si le mode sombre est actif dans le stockage local, alors il est activé dès l'ouverture de la  page
 if(darkmode === "active") enableDarkmode()
 
+// Lorsque le bouton est clické :
+// - la valeur du mode sombre est changée dans le stockage local
+// - si le mode sombre était activé, alors il est désactivé, sinon, il est activé
 themeSwitch.addEventListener("click", ()=>{
     darkmode = localStorage.getItem('darkmode')
     darkmode !== "active" ? enableDarkmode() : disableDarkmode()
